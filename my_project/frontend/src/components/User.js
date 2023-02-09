5e4d78165ee403e7da91a86cde0b4f902c680de4
New_User <New_User@user.ru>
2023-02-09 11:28:53 +0300

    lesson_5

--- my_project/frontend/src/components/User.js
+++ my_project/frontend/src/components/User.js
@@ -18,7 +18,6 @@ const UserItem = ({user}) => {
             </td>
         </tr>
     )
-
 }
 
 
@@ -26,22 +25,19 @@ const UserList = ({users}) => {
     return (
         <table className="table">
             <thead className="table-dark">
-            <table className="table">
-                <thead className="table-dark">
-                <tr>
-                    <th scope="col">Электронный адрес</th>
-                    <th scope="col">Username</th>
-                    <th scope="col">Имя</th>
-                    <th scope="col">Фамилия</th>
-                </tr>
-                </thead>
-                <tbody>
-                    {users.map((user) => <UserItem user={user}/>)}
-                    {users.map((user) => <UserItem user={user} key={`user${user.username}`}/>)}
-                </tbody>
-            </table>
-        )
+            <tr>
+                <th scope="col">Электронный адрес</th>
+                <th scope="col">Username</th>
+                <th scope="col">Имя</th>
+                <th scope="col">Фамилия</th>
+            </tr>
+            </thead>
+            <tbody>
+            {users.map((user) => <UserItem user={user}/>)}
+            {users.map((user) => <UserItem user={user} key={`user${user.username}`}/>)}
+            </tbody>
+        </table>
+    )
 }
-
 
 export default UserList
